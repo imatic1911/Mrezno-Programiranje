dbeaa9cbbb6b39a1e36876e8df565901e0654209
imatic1911 <imatic1911@gmail.com>
2024-05-13 15:27:16 +0200

    Vjezba 3,4,5

---
+++ Vjezba 4-Ivana Matic/Zad1/zadatak_1.py
@@ -0,0 +1,18 @@
+import socket
+
+
+client_socket = socket.socket()
+host = socket.gethostname()
+host_ip = socket.gethostbyname(host)
+port = 9999
+
+message = b"Hello, World!" 
+
+print("UDP target IP: %s" % host_ip)
+print("UDP target port: %s" % port)
+print("Message: %s" % message)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+sock.sendto(message, (host, port))
